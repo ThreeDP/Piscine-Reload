246d97f8553241d7d37ecfab0e225b2b2cf52507
Threedp <davypaulinocbjr@aluno.ifsp.edu.br>
2022-08-25 15:36:40 -0300

    factorial iterative

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,11 +6,21 @@
 /*   By: dapaulin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 21:11:09 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/08/23 21:12:01 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/08/25 15:02:39 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
-	
+	int	result;
+
+	result = 1;
+	if (nb < 0)
+		return (0);
+	while (nb > 0)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
 }
