c91a3033773183f6a1888fbdd951456c8b52b472
Threedp <davypaulinocbjr@aluno.ifsp.edu.br>
2022-08-25 15:37:20 -0300

    quadradinho

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,8 +6,22 @@
 /*   By: dapaulin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 11:41:43 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/08/25 11:55:38 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/08/25 15:30:40 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_sqrt_aux(int nb, int sqrt)
+{
+	if (nb < 0)
+		return (0);
+	else if (sqrt >= 46340)
+		return (sqrt);
+	else if (sqrt * sqrt == nb)
+		return (sqrt);
+	return (ft_sqrt_aux(nb, sqrt + 1));
+}
 
+int	ft_sqrt(int nb)
+{
+	return (ft_sqrt_aux(nb, 0));
+}
