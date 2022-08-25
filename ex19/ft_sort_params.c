f7f700512e8c9f931b3df43b1a2ca6d3df619a1e
Threedp <davypaulinocbjr@aluno.ifsp.edu.br>
2022-08-25 15:38:17 -0300

    ordem params

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dapaulin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 18:02:35 by dapaulin          #+#    #+#             */
+/*   Updated: 2022/08/25 13:36:26 by dapaulin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	start;
+	int	cursor;
+	int	i;
+
+	start = 1;
+	while (start < argc)
+	{
+		cursor = start + 1;
+		while (cursor < argc)
+		{
+			if (ft_strcmp(argv[start], argv[cursor]) > 0)
+				ft_swap(argv + start, argv + cursor);
+			cursor++;
+		}
+		start++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
