128081a3a255f71a136678fdfd9866bcfa67d112
threedp.png <davypaulinocbjr@outlook.com>
2022-09-02 10:35:56 +0200

    Acho que agora aprendi malloc

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *str;
+	int i;
+
+	str = (char *) malloc(ft_strlen(src) + 1);
+	if (!str)
+		return (NULL);
+	while (src[i])
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return(str);
+}
